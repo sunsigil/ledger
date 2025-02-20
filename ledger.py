4cb717554b4a84cb8d7ba30e1e6b66384d1a5eb2
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-02-20 12:22:54 -0600

    Updated to use imgui bundle

--- ledger.py
+++ ledger.py
@@ -1,16 +1,29 @@
+#!/usr/bin/env python3
+
 import csv;
 import configparser;
 import os;
-
 import OpenGL;
 OpenGL.FULL_LOGGING = True;
 from OpenGL.GL import *;
-
 import glfw;
-import imgui;
-from imgui.integrations.glfw import GlfwRenderer;
-
+from imgui_bundle import imgui;
+import ctypes;
+from imgui_bundle.python_backends.glfw_backend import GlfwRenderer;
+from pathlib import Path;
 from cowtools import *;
+
+
+config = configparser.ConfigParser();
+config.read("ledger.ini");
+default_actors = config["CONFIG"]["default_actors"].split(", ");
+default_actors = [x.lower() for x in default_actors];
+sales_tax = float(config["CONFIG"]["sales_tax"]);
+commie_list = config["CONFIG"]["commie_list"].split(", ");
+commie_list = [x.lower() for x in commie_list];
+width = int(config["CONFIG"]["width"]);
+height = int(config["CONFIG"]["height"]);
+
 
 class Purchase:
     def __init__(self, item, cost, quantity, taxed):
@@ -37,6 +50,7 @@ class Division:
         if actor in self.actors:
             return 1.0/float(len(self.actors));
         return 0.0;
+
 
 class Ledger:
     def __init__(self):
@@ -88,79 +102,72 @@ class Ledger:
                 count += 1;
         return 100 * (count / len(self.purchases));
 
-config = configparser.ConfigParser();
-config.read("ledger.ini");
-default_actors = config["CONFIG"]["default_actors"].split(", ");
-default_actors = [x.lower() for x in default_actors];
-sales_tax = float(config["CONFIG"]["sales_tax"]);
-commie_list = config["CONFIG"]["commie_list"].split(", ");
-commie_list = [x.lower() for x in commie_list];
-width = int(config["CONFIG"]["width"]);
-height = int(config["CONFIG"]["height"]);
-
-ledger_path = None;
 ledger = None;
+		
 
+def glfw_error_callback(error, description):
+ 	print(f"[GLFW ERROR] {error}: {description}");
+glfw.set_error_callback(glfw_error_callback);
 if not glfw.init():
-    print("Failed to initialize GLFW. Exiting");
-    exit();
+	print("Failed to initialize GLFW. Exiting");
+	exit();
 glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3);
 glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3);
 glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE);
 glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE);
 handle = glfw.create_window(width, height, "Ledger", None, None);
 if not handle:
-    print("Failed to create window. Exiting");
-    glfw.terminate();
-    exit();
+	print("Failed to create window. Exiting");
+	glfw.terminate();
+	exit();
 glfw.make_context_current(handle);
-print("Renderer:", glGetString(GL_RENDERER));
-print("GL Version:", glGetString(GL_VERSION));
-print("SL Version:", glGetString(GL_SHADING_LANGUAGE_VERSION));
+print("Renderer:", glGetString(GL_RENDERER).decode("utf-8"));
+print("GL Version:", glGetString(GL_VERSION).decode("utf-8"));
+print("SL Version:", glGetString(GL_SHADING_LANGUAGE_VERSION).decode("utf-8"));
 
 imgui.create_context();
 imgui_io = imgui.get_io();
-imgui_io.display_size = width, height;
-imgui_io.fonts.get_tex_data_as_rgba32();
-imgui_renderer = GlfwRenderer(handle);
-imgui_flag_list = [
-    imgui.WINDOW_NO_SAVED_SETTINGS,
-    imgui.WINDOW_NO_MOVE,
-    imgui.WINDOW_NO_RESIZE,
-    imgui.WINDOW_NO_NAV_INPUTS,
-    imgui.WINDOW_NO_NAV_FOCUS,
-    imgui.WINDOW_NO_COLLAPSE,
-    imgui.WINDOW_NO_BACKGROUND,
-    imgui.WINDOW_NO_BRING_TO_FRONT_ON_FOCUS
+imgui_io.config_windows_move_from_title_bar_only = True;
+imgui.style_colors_dark()
+impl = GlfwRenderer(handle);
+
+window_flag_list = [
+	imgui.WindowFlags_.no_saved_settings,
+	imgui.WindowFlags_.no_move,
+	imgui.WindowFlags_.no_resize,
+	imgui.WindowFlags_.no_nav_inputs,
+	imgui.WindowFlags_.no_nav_focus,
+	imgui.WindowFlags_.no_collapse,
+	imgui.WindowFlags_.no_background,
+	imgui.WindowFlags_.no_bring_to_front_on_focus,
 ];
-imgui_flags = foldl(lambda a, b : a | b, 0, imgui_flag_list);
+window_flags = foldl(lambda a, b : a | b, 0, window_flag_list);
 
 while not glfw.window_should_close(handle):
     glfw.poll_events();
-    imgui_renderer.process_inputs();
+    impl.process_inputs();
 
     glEnable(GL_PROGRAM_POINT_SIZE);
     glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
 
     imgui.new_frame();
-    imgui.set_next_window_position(0, 0);
-    imgui.set_next_window_size(width, height);
-    imgui.begin("Ledger", None, flags=imgui_flags);
+    imgui.set_next_window_pos((0, 0));
+    imgui.set_next_window_size((width, height));
+    imgui.begin("Ledger", None, flags=window_flags);
     
     if imgui.begin_main_menu_bar():
         if imgui.begin_menu("File"):
             if imgui.begin_menu("Open"):
-                receipt_dir = "./receipts";
-                for path in os.listdir(receipt_dir):
+                for path in os.listdir("receipts"):
                     if path.endswith(".csv"):
-                        if imgui.menu_item(path)[0]:
-                            ledger_path = os.path.join(receipt_dir, path);
+                        if imgui.menu_item_simple(path):
+                            ledger_path = os.path.join("receipts", path);
                             ledger = Ledger();
                             with open(ledger_path) as file:
                                 reader = csv.reader(file);
                                 for row in reader:
-                                    purchase = Purchase(row[0], float(row[1]), int(row[2]), False);
+                                    purchase = Purchase(row[0], float(row[1]), int(row[2]) if len(row) > 2 else 1, False);
                                     ledger.add_purchase(purchase);
                                     if purchase.item in commie_list:
                                         for actor in default_actors:
@@ -219,8 +226,8 @@ while not glfw.window_should_close(handle):
                 imgui.end_tab_item();
 
             if imgui.begin_tab_item("Tables")[0]:
-                if imgui.collapsing_header("Receipt")[0]:
-                    if imgui.begin_table("Receipt Table", 3, imgui.TABLE_BORDERS | imgui.TABLE_SIZING_FIXED_SAME):
+                if imgui.collapsing_header("Receipt"):
+                    if imgui.begin_table("Receipt Table", 3, imgui.TableFlags_.borders | imgui.TableFlags_.sizing_fixed_same):
                         imgui.table_setup_column("Item");
                         imgui.table_setup_column("Cost");
                         imgui.table_setup_column("Quantity");
@@ -241,8 +248,8 @@ while not glfw.window_should_close(handle):
                         imgui.text(f"Total: ${total + tax:.2f}");
                 imgui.separator();
                 for actor in default_actors:
-                    if imgui.collapsing_header(actor)[0]:
-                        if imgui.begin_table(f"{actor} Table", 4, imgui.TABLE_BORDERS | imgui.TABLE_SIZING_FIXED_SAME):
+                    if imgui.collapsing_header(actor):
+                        if imgui.begin_table(f"{actor} Table", 4, imgui.TableFlags_.borders | imgui.TableFlags_.sizing_fixed_same):
                             imgui.table_setup_column("Item");
                             imgui.table_setup_column("Cost");
                             imgui.table_setup_column("Quantity");
@@ -274,7 +281,7 @@ while not glfw.window_should_close(handle):
     
     imgui.end();
     imgui.render();
-    imgui_renderer.render(imgui.get_draw_data());
+    impl.render(imgui.get_draw_data());
     imgui.end_frame();
 
     glfw.swap_buffers(handle);
